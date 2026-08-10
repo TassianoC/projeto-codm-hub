4a2dc5d868b71caeecaf18068951507e33472e59
TassianoC <tassianoops@gmail.com>
2026-08-10 19:30:19 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+/**
+ * Configuração pública do projeto Firebase.
+ * As chaves abaixo identificam o projeto web; a segurança real está nas Rules
+ * e nas Cloud Functions. Nunca coloque OPENAI_API_KEY ou segredos neste arquivo.
+ */
+export const firebaseConfig = {
+  apiKey: "AIzaSyBnysGMTtMQo0RbmEMjFPhBjZVLzovbgaA",
+  authDomain: "projeto-codm-hub.firebaseapp.com",
+  projectId: "projeto-codm-hub",
+  storageBucket: "projeto-codm-hub.firebasestorage.app",
+  messagingSenderId: "1038952355133",
+  appId: "1:1038952355133:web:18f011328d2e111316a154"
+};
